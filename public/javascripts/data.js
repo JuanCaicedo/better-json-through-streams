73f32745c7b9aeb1da98c02427a9ca346d7e2703
Juan Caicedo <juan@spanishdict.com>
2015-11-07 20:24:51 -0500

    done notification

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -11,25 +11,10 @@ function getCell(container, x, y) {
 
 oboe('http://localhost:3000/data')
   .node('{x y color}', function(point) {
-    // debugger
     var grid = document.querySelector('.grid');
     var cell = getCell(grid, point.x, point.y);
     cell.classList.add(point.color);
+  })
+  .done(function(){
+    document.querySelector('#status-message').textContent = 'All data Loaded!';
   });
-//     .node('{cards}', function(item) {
-//       count += 1;
-//       $('.data').text(count);
-//       ctx.setName = item.name;
-//       if (item.name !== 'Legends') {
-//         return oboe.drop;
-//       }
-//     })
-//     .node('cards.[*]', function(card) {
-//       if (ctx.setName === 'Legends') {
-//         cards.push(card)
-//         $('.cards').text(cards.length);
-//       }
-//     })
-// .done(function(sets) {
-//   $('.sets').text(sets.length);
-// })
