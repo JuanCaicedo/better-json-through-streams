581882144e2210691721b61422b914c98d57d1bd
Juan Caicedo <juan@spanishdict.com>
2015-11-07 19:54:47 -0500

    no jquery

--- public/javascripts/data.js
+++ public/javascripts/data.js
@@ -1,38 +1,35 @@
-function getByIndex(element, index){
-  var container = $(element);
-  var child = container.children()[index];
+function getByIndex(container, index) {
+  var child = container.children[index];
   return child;
 }
 
-function getCell(x, y) {
-  var grid = $('.wrapper');
-  var row = getByIndex(grid, y);
+function getCell(container, x, y) {
+  var row = getByIndex(container, y);
   var cell = getByIndex(row, x);
   return cell;
 }
 
-$(document).ready(function() {
-  //   var ctx = {};
-  oboe('http://localhost:3000/data')
-    .node('{x y color}', function(point){
-      var cell = getCell(point.x, point.y);
-      $(cell).addClass(point.color);
-    })
-    //     .node('{cards}', function(item) {
-    //       count += 1;
-    //       $('.data').text(count);
-    //       ctx.setName = item.name;
-    //       if (item.name !== 'Legends') {
-    //         return oboe.drop;
-    //       }
-    //     })
-    //     .node('cards.[*]', function(card) {
-    //       if (ctx.setName === 'Legends') {
-    //         cards.push(card)
-    //         $('.cards').text(cards.length);
-    //       }
-    //     })
-    // .done(function(sets) {
-    //   $('.sets').text(sets.length);
-    // })
-});
+oboe('http://localhost:3000/data')
+  .node('{x y color}', function(point) {
+    // debugger
+    var grid = document.querySelector('.grid');
+    var cell = getCell(grid, point.x, point.y);
+    cell.classList.add(point.color);
+  });
+//     .node('{cards}', function(item) {
+//       count += 1;
+//       $('.data').text(count);
+//       ctx.setName = item.name;
+//       if (item.name !== 'Legends') {
+//         return oboe.drop;
+//       }
+//     })
+//     .node('cards.[*]', function(card) {
+//       if (ctx.setName === 'Legends') {
+//         cards.push(card)
+//         $('.cards').text(cards.length);
+//       }
+//     })
+// .done(function(sets) {
+//   $('.sets').text(sets.length);
+// })
