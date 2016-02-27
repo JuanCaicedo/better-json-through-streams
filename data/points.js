89e5e4803a3a932cca401818357335147a74026b
Juan Caicedo <juan@spanishdict.com>
2016-02-27 11:08:30 -0500

    read file with highland and oboe

--- data/points.js
+++ data/points.js
@@ -1,32 +1,24 @@
 var highland = require('highland');
-
-var point1 = {
-  "index": 105,
-  "action": "draw",
-  "x": 9,
-  "y": 7,
-  "originalColor": "transparent",
-  "color": "orange",
-  "size": 1,
-  "drawPathId": 1446859159445
-};
-
-var point2 = {
-  "index": 105,
-  "action": "draw",
-  "x": 9,
-  "y": 7,
-  "originalColor": "transparent",
-  "color": "orange",
-  "size": 1,
-  "drawPathId": 1446859159445
-};
+var oboe = require('oboe');
+var fs = require('fs');
+var path = require('path');
+var filePath = path.resolve(__dirname, '../points.json');
+var readStream = fs.createReadStream(filePath);
 
 function getStream() {
-  return highland([
-    point1,
-    point2
-  ]);
+  var dataStream = highland(readFile);
+  return dataStream;
+}
+
+function readFile(push, next) {
+  var fileStream = fs.createReadStream(filePath);
+  oboe(fileStream)
+    .node('{x y color}', function(point) {
+      push(null, point);
+    })
+    .done(function() {
+      push(null, highland.nil);
+    });
 }
 
 module.exports = {
