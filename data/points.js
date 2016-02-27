1398d3d20905b62d2be162ebf42ef4b1748a4831
Juan Caicedo <juan@spanishdict.com>
2016-02-27 16:54:14 -0500

    merge two streams

--- data/points.js
+++ data/points.js
@@ -2,24 +2,32 @@ var highland = require('highland');
 var oboe = require('oboe');
 var fs = require('fs');
 var path = require('path');
-var filePath = path.resolve(__dirname, '../points.json');
-var readStream = fs.createReadStream(filePath);
+
+function getPointStream(filePath) {
+  return highland(function(push, next) {
+    var fileStream = fs.createReadStream(filePath);
+    oboe(fileStream)
+      .node('{x y color}', function(point) {
+        push(null, point);
+      })
+      .done(function() {
+        push(null, highland.nil);
+      });
+  });
+}
 
 function getStream() {
-  var dataStream = highland(readFile);
-  return dataStream;
+  var catPath = path.resolve(__dirname, './cat-points.json');
+  var sunPath = path.resolve(__dirname, './sun-points.json');
+
+  var catStream = getPointStream(catPath);
+  var sunStream = getPointStream(sunPath);
+  return highland([
+    sunStream,
+    catStream
+  ]).merge();
 }
 
-function readFile(push, next) {
-  var fileStream = fs.createReadStream(filePath);
-  oboe(fileStream)
-    .node('{x y color}', function(point) {
-      push(null, point);
-    })
-    .done(function() {
-      push(null, highland.nil);
-    });
-}
 
 module.exports = {
   getStream: getStream
